879d7ef54c36b4757496ee12ac56ba5e972f5f81
Driftedbucket <ndinowix@gmail.com>
2026-05-01 19:42:02 +0200

    Function signatures

--- Icr.java
+++ Icr.java
@@ -1,4 +1,9 @@
+import java.util.ArrayList;
 
 public class Icr {
-    
+
+    private String newTempVariable(){}
+    private String generate(){}
+    private String parseExpression(){}
+    private String parseTerm(){}
 }
