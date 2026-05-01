4fc1ac33bd23f44149c4f710a62e844d56335da5
Driftedbucket <ndinowix@gmail.com>
2026-05-01 19:50:49 +0200

    Retrieve icr and split it by the assign operator. Variable to the left and expression to the right

--- Codegen.java
+++ Codegen.java
@@ -1,4 +1,19 @@
 
 public class Codegen {
     
+    public String generate(String icrOutput){
+        String[] icrlines = icrOutput.split("\n");
+        StringBuilder assemblyCode = new StringBuilder();
+
+        for(String icrline:icrlines){
+            icrline=icrline.trim();
+            if(icrline.isEmpty()) continue;
+
+            String[] parts = icrline.split("=",2);
+            if(parts.length<2)continue;
+
+            String dest = parts[0].trim();
+            String expr = parts[1].trim();
+    }
+}
 }
