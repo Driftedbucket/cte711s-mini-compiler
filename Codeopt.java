3b947dfbfa9a2b31d271e21da1cde15d72c17ea8
Driftedbucket <ndinowix@gmail.com>
2026-05-01 19:57:16 +0200

    Split into individual instructions and trakc last sotre destination

--- Codeopt.java
+++ Codeopt.java
@@ -1,4 +1,10 @@
 
 public class Codeopt {
-    
+    public String optimise(String asmCode){
+
+        String[] lines = asmCode.split("\n");
+        StringBuilder optimised = new StringBuilder();
+
+        String prevStore = "";  
+    }
 }
