c60ff0426e1c0d5180ee8c8c09293c2b6a7b22f6
Driftedbucket <ndinowix@gmail.com>
2026-05-01 19:46:50 +0200

    Generate function implementation. Uses the 2 helper functions to generate the intermediate code(t1=m+a)

--- Icr.java
+++ Icr.java
@@ -3,7 +3,35 @@ import java.util.ArrayList;
 public class Icr {
 
     private String newTempVariable(){}
-    private String generate(){}
+    private String generate(String line){
+        //split LET keyword fi rpresent so we see LET G = A + b as G = A + b
+            line=line.trim().replace("LET","");
+
+            //split on the = to get the variable annd expression
+            // M=A+B
+            //parts = ["M","A+B"]
+            String[] parts = line.split("=",2);
+            if(parts.length<2){
+                return "Cannot generate ICR for: "+line;
+            }
+
+            String variable = parts[0].trim();
+            String expression = parts[1].trim();
+
+            ArrayList<String> icrlines = new ArrayList<>();
+            count =0;
+
+            //parse expr left to right, respecting * abnd / before (+ and -)
+            String result = parseExpression(expression, icrlines);
+
+            icrlines.add(variable+" = "+result);
+
+            StringBuilder sb = new StringBuilder();
+            for(String icrline: icrlines){
+                sb.append("    ").append(icrline).append("\n");
+            }
+            return sb.toString();
+    }
     private String parseExpression(String expression, ArrayList<String> icrlines){
         expression=expression.trim();
 
