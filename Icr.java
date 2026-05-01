1fd01d4e20b88627088d70057ae4d81747ea7e1a
Driftedbucket <ndinowix@gmail.com>
2026-05-01 19:43:35 +0200

    ParseTerm function implementation

--- Icr.java
+++ Icr.java
@@ -5,5 +5,29 @@ public class Icr {
     private String newTempVariable(){}
     private String generate(){}
     private String parseExpression(){}
-    private String parseTerm(){}
+    
+    private String parseTerm(String expression, ArrayList<String> icrlines){
+        expression=expression.trim();
+
+            int splitAt=-1;
+            for(int i=0; i<expression.length(); i++){
+                if(expression.charAt(i)=='*'||expression.charAt(i)=='/'){
+                    splitAt=i;
+                }
+            }
+            if(splitAt==-1){
+                return expression.trim();
+            }
+
+            String leftExpr = expression.substring(0, splitAt).trim();
+            char operator = expression.charAt(splitAt);
+            String rightExpr = expression.substring(0, splitAt).trim();
+            
+            String leftResult = leftExpr.isEmpty()?leftExpr:parseTerm(leftExpr, icrlines);
+            String rightResult = rightExpr.trim();
+            
+            String temp = newTempVariable();
+            icrlines.add(temp+"="+leftResult+" "+operator+""+rightResult);
+            return temp;
+    }
 }
