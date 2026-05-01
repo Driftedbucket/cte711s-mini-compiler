aa18b182f6bf08d17fc21eb97a6725607e02ec91
Driftedbucket <ndinowix@gmail.com>
2026-05-01 19:44:50 +0200

    ParseExpression function implementation

--- Icr.java
+++ Icr.java
@@ -4,8 +4,32 @@ public class Icr {
 
     private String newTempVariable(){}
     private String generate(){}
-    private String parseExpression(){}
-    
+    private String parseExpression(String expression, ArrayList<String> icrlines){
+        expression=expression.trim();
+
+            int splitAt=-1;
+            for(int i=0; i<expression.length(); i++){
+                if(expression.charAt(i)=='*'||expression.charAt(i)=='/'){
+                    splitAt=i;
+                }
+            }
+            if(splitAt==-1){
+                //no + or - pass dowen to term(hanldes * and /)
+                return parseTerm(expression, icrlines);
+            }
+
+            String leftExpr = expression.substring(0, splitAt).trim();
+            char operator = expression.charAt(splitAt);
+            String rightExpr = expression.substring(splitAt+1).trim();
+
+            String leftResult = parseTerm(leftExpr, icrlines);
+            String rightResult = parseTerm(rightExpr, icrlines);
+
+            String temp = newTempVariable();
+            icrlines.add(temp+" = "+leftResult+" "+operator+" "+rightResult);
+            return temp;
+    }
+
     private String parseTerm(String expression, ArrayList<String> icrlines){
         expression=expression.trim();
 
