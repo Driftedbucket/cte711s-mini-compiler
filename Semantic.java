516df2c4d31e8ba30a47580f9cab47df11833f34
angel2-star <ndinaunandapo@gmail.com>
2026-05-01 19:37:20 +0200

    Semantic analysis

--- Semantic.java
+++ Semantic.java
@@ -1,4 +1,17 @@
 
 public class Semantic {
-    
+    public class Semantic{
+
+    public String analyse(String line){
+        
+        char[] forbidden={'%','$','&','<','>'};
+
+        for(char badChar:forbidden){
+            if(line.indexOf(badChar) != -1){
+                return "SEMANTIC ERROR: Illegal symbol '"+ badChar+"' found in line. Try again!";
+            }
+        }
+        return "OK";
+    }
+}
 }
