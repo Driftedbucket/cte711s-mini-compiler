3914cbc839c094f63e119beb4f16af009621dfd9
Driftedbucket <ndinowix@gmail.com>
2026-05-03 18:41:25 +0200

    Source prgram lines

--- main.java
+++ main.java
@@ -2,5 +2,19 @@ public class main {
     
     public static void main(String[] args) {
         
+        String[] sourceLines = {
+            "BEGIN",
+            "INTEGER A, B, C, E, M, N, G, H, I, a, c",
+            "INPUT A, B, C",
+            "LET B = A */ M",
+            "LET G = a + c",
+            "temp = <s%**h - j / w +d +*$&;",
+            "M = A/B+C",
+            "N = G/H-I+a*B/c",
+            "WRITE M",
+            "WRITEE F;",
+            "END"
+        };
+
     }
 }
