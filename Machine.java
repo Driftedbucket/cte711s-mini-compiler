702e37f3cea99b8a0fb29d606252fca745d694ed
suspiciouskey <awikegulu@gmail.com>
2026-05-03 18:58:04 +0200

    Add Function that converts each character to binary(8-bit ASCII)

--- Machine.java
+++ Machine.java
@@ -1,4 +1,13 @@
 
 public class Machine {
-    
+
+    // Converts each character in the instruction to its 8-bit ASCII binary
+    private String toBin(String text) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : text.toCharArray()) {
+            sb.append(String.format("%8s", Integer.toBinaryString(c)).replace(' ', '0'));
+            sb.append(" ");
+        }
+        return sb.toString();
+    }
 }
